147183c98b130ecd9fed8298a99f456d91d5e49e
Andrés Cardenal Antón <andrescardenalanton@gmail.com>
2022-09-23 14:43:30 +0200

    Inicio
    Subida inicial del proyecto

---
+++ ICI_Practica1_v1.0/src/es/ucm/fdi/ici/c2223/practica1/grupo/pacman/GhostsAlphaBeta.java
@@ -0,0 +1,17 @@
+package es.ucm.fdi.ici.c2223.practica1.grupo.pacman;
+
+import pacman.game.Game;
+
+import java.util.EnumMap;
+
+import pacman.controllers.GhostController;
+import pacman.game.Constants.GHOST;
+import pacman.game.Constants.MOVE;
+
+public class GhostsAlphaBeta extends GhostController {
+
+	@Override
+	public EnumMap<GHOST, MOVE> getMove(Game game, long timeDue) {
+		return null;
+	}
+}
