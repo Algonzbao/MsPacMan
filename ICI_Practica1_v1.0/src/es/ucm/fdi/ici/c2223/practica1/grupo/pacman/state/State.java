456395289e2485002b38ea229be14133aeb8a214
Colevo <algonz22@ucm.es>
2022-10-06 21:28:18 +0200

    Creacion en State

--- ICI_Practica1_v1.0/src/es/ucm/fdi/ici/c2223/practica1/grupo/pacman/state/State.java
+++ ICI_Practica1_v1.0/src/es/ucm/fdi/ici/c2223/practica1/grupo/pacman/state/State.java
@@ -1,9 +1,11 @@
 package es.ucm.fdi.ici.c2223.practica1.grupo.pacman.state;
 
+
 import java.util.List;
 import java.util.Map;
 
 import es.ucm.fdi.ici.c2223.practica1.grupo.pacman.explorer.Agente;
+import pacman.game.Game;
 
 public class State {
 	
@@ -18,36 +20,57 @@ public class State {
 	private final List<Douposition> powerPills;
 	private boolean hasPower;
 	private Integer points;
+	private Game game;
 	
-	public State(List<Camino> maze, Map<Agente, Position> agentes, List<Douposition> pills, List<Douposition> powerPills) {
+	public State(List<Camino> maze, Map<Agente, Position> agentes, List<Douposition> pills, List<Douposition> powerPills, Game game) {
 		this.maze = maze;
 		this.agentes = agentes;
 		this.pills = pills;
 		this.powerPills = powerPills;
 		this.points = 0;
 		this.hasPower = false;
+		this.game = game;
 		
 	}
 	
-	
-	public void hasPowerPill() {
+	public boolean hasPowerPill(Camino c) {
 		//Atributo booleando de si tiene powerpill,
-		for(Camino c: getMaze()) {
+		boolean hasPower = false;
 			if(c.getpPillPos() > 0) {
 				this.hasPower = true;
-			}
 		}
+			return hasPower;
 	}
-	public void amountPills() {
+	
+	public Integer amountPills(Camino c) {
 		//atributo de cuantas pills hay 
-		for(Camino c: getMaze()) {
 			this.points = c.getPills();
-			}
+		return points;
 	}
-	public void whoWins() {
+	
+	public Integer whoWins(Camino c) {
 		// saber quien llega antes a la pill
 		//for all the agents ver si en el camino hay alguna ppill y si la hay ver cuánto tarda
-		//si hay fantasma y pacman en el mismo camino entonces calcular cuál de los dos llega antes
+		//si hay fantasma y pacman en el mismo camino entonces calcular cuál de los dos llega antesç
+		Integer distancePacman = 0, distanceGhost = Integer.MAX_VALUE;
+		  for (Map.Entry<Agente,Position> ag : agentes.entrySet()) {
+	             if(ag.getKey() == Agente.PACMAN) {
+	            	if(hasPowerPill(ag.getValue().getCamino())) {
+	            		distancePacman = game.getManhattanDistance(ag.getValue().getPlace(), c.getpPillPos());
+	            	}
+	             }
+	             else {
+	            	 if(hasPowerPill(ag.getValue().getCamino())) {
+		            		if(distanceGhost < game.getManhattanDistance(ag.getValue().getPlace(), c.getpPillPos())) {
+		            			distanceGhost = game.getManhattanDistance(ag.getValue().getPlace(), c.getpPillPos());
+		            		}
+	            	 }	
+	            }
+	            
+		  }
+		  //valor minimo, me come el fantasma valor maximo, me lo como
+		  Integer result = distancePacman >= distanceGhost ? Integer.MIN_VALUE : Integer.MAX_VALUE; 
+         	return result;
 	}
 	
 	public List<Camino> getMaze() {
