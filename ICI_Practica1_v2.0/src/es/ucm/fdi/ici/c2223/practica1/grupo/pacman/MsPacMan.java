3dcb685940245b368ef9ad6fad07db12390a2e86
Colevo <algonz22@ucm.es>
2022-10-09 17:18:36 +0200

    Fantasma comestible

--- ICI_Practica1_v2.0/src/es/ucm/fdi/ici/c2223/practica1/grupo/pacman/MsPacMan.java
+++ ICI_Practica1_v2.0/src/es/ucm/fdi/ici/c2223/practica1/grupo/pacman/MsPacMan.java
@@ -20,4 +20,18 @@ public class MsPacMan extends PacmanController {
 	public MOVE getMove(Game game, long timeDue) {
 		return Calculator.getNextMove(Agente.PACMAN);
 	}
+	public MOVE getMove(Game game, long timeDue) {
+		this.game=game;
+		
+		//si hay fantasmas comestibles y son el mas cercano los priorizamos porque son la fuente principal de puntos
+		GHOST edibleGhost = getNearestEdibleGhost(GHOSTS_VISIBILITY_FOLLOW_LIMIT);
+		if(edibleGhost != null && notExistAnyCloserGhost(edibleGhost)) {
+			return game.getApproximateNextMoveTowardsTarget(game.getPacmanCurrentNodeIndex(), game.getGhostCurrentNodeIndex(edibleGhost), game.getPacmanLastMoveMade(), DM.PATH);
+		}
+		
+		//si no huimos por las rutas sin fantasmas hacia pills o powerpills (si quedan)
+		else {
+			return runAway();
+		}
+	}
 }
