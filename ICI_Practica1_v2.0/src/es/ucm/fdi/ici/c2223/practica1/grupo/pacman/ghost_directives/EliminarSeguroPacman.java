b364f7654ce953ae10198f34e3c5b8ba8da67538
Andrés Cardenal Antón <andrescardenalanton@gmail.com>
2022-10-09 19:51:24 +0200

    detective

--- ICI_Practica1_v2.0/src/es/ucm/fdi/ici/c2223/practica1/grupo/pacman/ghost_directives/EliminarSeguroPacman.java
+++ ICI_Practica1_v2.0/src/es/ucm/fdi/ici/c2223/practica1/grupo/pacman/ghost_directives/EliminarSeguroPacman.java
@@ -25,47 +25,4 @@ public class EliminarSeguroPacman extends GhostDirective {
 		}
 		isInTheWay()
 	}
-	public static Boolean isInTheWay(Integer fromIndex, Integer toIndex, Integer index, MOVE lastMove) {
-		// lastNode FALTA
-		GameContainer.get().getNextMoveAwayFromTarget(0, 0, null)
-		if (GameContainer.get().isJunction(index))
-			return false;
-		Integer lastNode, actualNode, nextNode;
-		for (int i = 0; i < 2; i++) {
-			lastNode = index;
-			actualNode = GameContainer.get().getNeighbouringNodes(index)[i];
-			while (!GameContainer.get().isJunction(actualNode)) {
-				nextNode = GameContainer.get().getNeighbouringNodes(actualNode)[0];
-				if (nextNode == lastNode)
-					nextNode = GameContainer.get().getNeighbouringNodes(actualNode)[1];
-				lastNode = actualNode;
-				actualNode = nextNode;
-			}
-			if (actualNode != fromIndex && actualNode != toIndex)
-				return false;
-		}
-		return lastMove == GameContainer.get().getNextMoveAwayFromTarget(fromIndex, toIndex, DM.PATH);
-	}
-	
-	public static Integer nextJunction(Integer index, MOVE lastMove) {
-		if (GameContainer.get().isJunction(index))
-			return index;
-		Integer[] junctions = new Integer[2];
-		Integer lastNode, actualNode, nextNode;
-		for (int i = 0; i < 2; i++) {
-			lastNode = index;
-			actualNode = GameContainer.get().getNeighbouringNodes(index)[i];
-			while (!GameContainer.get().isJunction(actualNode)) {
-				nextNode = GameContainer.get().getNeighbouringNodes(actualNode)[0];
-				if (nextNode == lastNode)
-					nextNode = GameContainer.get().getNeighbouringNodes(actualNode)[1];
-				lastNode = actualNode;
-				actualNode = nextNode;
-			}
-			junctions[i] = actualNode;
-		}
-		if (lastMove == GameContainer.get().getNextMoveAwayFromTarget(junctions[0], junctions[1], DM.PATH))
-			return junctions[0];
-		return  junctions[1];
-	}
 }
