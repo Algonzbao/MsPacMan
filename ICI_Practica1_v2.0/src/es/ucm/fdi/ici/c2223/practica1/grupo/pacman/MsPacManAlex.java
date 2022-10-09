94d8a9ead4d8f631c2fd9d5501490970e5bd3647
Colevo <algonz22@ucm.es>
2022-10-09 18:54:54 +0200

    Punto Medio

--- ICI_Practica1_v2.0/src/es/ucm/fdi/ici/c2223/practica1/grupo/pacman/MsPacManAlex.java
+++ ICI_Practica1_v2.0/src/es/ucm/fdi/ici/c2223/practica1/grupo/pacman/MsPacManAlex.java
@@ -1,7 +1,4 @@
 package es.ucm.fdi.ici.c2223.practica1.grupo.pacman;
-import java.util.HashMap;
-import java.util.Map;
-
 import pacman.controllers.PacmanController;
 import pacman.game.Constants.DM;
 import pacman.game.Constants.GHOST;
@@ -9,8 +6,6 @@ import pacman.game.Constants.MOVE;
 import pacman.game.Game;
 	
 public class MsPacManAlex extends PacmanController{
-	
-	private static final DM dm = DM.PATH;
 		private Game game;
 		private final int GHOSTS_NUMBER = 4;
 		private final int GHOSTS_FOLLOW_LIMIT = 150;
@@ -21,25 +16,23 @@ public class MsPacManAlex extends PacmanController{
 		
 		//Si me puedo comer un fantasma y es el que más cerca tengo de todos me lo como (max puntos)
 		GHOST edibleGhost = getNearestEdibleGhost(GHOSTS_FOLLOW_LIMIT);
-		if (edibleGhost == null)
-			return null;
-		if(edibleGhost != null) {
-			int pacmanIndex = game.getPacmanCurrentNodeIndex();
-			int edibleGhostIndex = game.getGhostCurrentNodeIndex(edibleGhost);
-			
+		if(edibleGhost != null && !isThereCloserGhost(edibleGhost)) {
 			return game.getApproximateNextMoveTowardsTarget(game.getPacmanCurrentNodeIndex(), game.getGhostCurrentNodeIndex(edibleGhost), game.getPacmanLastMoveMade(), DM.PATH);
+		}
+		
+		//huir
+		else {
+			return HUIR();
 		}
 	}
 	private GHOST getNearestEdibleGhost(int limit) {
 		int [] ghostPositions = new int[GHOSTS_NUMBER];
 		
 		int i=0;
-		Map<Integer, GHOST> map = new HashMap<>();
 		//guardar la posicion en el mapa de cada uno de los fantasmas comestibles
 		for (GHOST ghostType : GHOST.values()) {
 			if(game.isGhostEdible(ghostType)) {
 				ghostPositions[i]=game.getGhostCurrentNodeIndex(ghostType);
-				map.put(ghostPositions[i], ghostType);
 				i++;
 			}
 		}
@@ -48,9 +41,24 @@ public class MsPacManAlex extends PacmanController{
 		
 		GHOST closestEat=null;
 		//si el fantasma está a la distacia más allá del límite que he establecido entonces no llego, en caso de que esté dentro del rango ese es el más cercano
-		if (game.getDistance(closestNode,game.getPacmanCurrentNodeIndex(),DM.PATH) <= limit)
-			closestEat = map.get(closestNode);
+		for (GHOST ghostType : GHOST.values()) {
+			if(game.getDistance(closestNode,game.getPacmanCurrentNodeIndex(),DM.PATH)==closestNode && game.getGhostCurrentNodeIndex(ghostType) <= limit) {
+				closestEat = ghostType;
+				break;
+			}
+		}
 		return closestEat;
+	}
+	
+	private boolean isThereCloserGhost(GHOST nearestGhost) {
+		//En caso de que exista algún fantasma no comestible más cerca que yo, me escapo
+		double nodeDistance = game.getDistance(game.getPacmanCurrentNodeIndex(), game.getGhostCurrentNodeIndex(nearestGhost), DM.PATH);
+		for(GHOST g: GHOST.values()) {
+			if(game.getDistance(game.getPacmanCurrentNodeIndex(), game.getGhostCurrentNodeIndex(g), DM.PATH) < nodeDistance &&  g !=nearestGhost && game.isGhostEdible(g) == false) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 }
--- ICI_Practica1_v2.0/src/es/ucm/fdi/ici/c2223/practica1/grupo/pacman/directivas/IrPuntoMedioConPacman.java
+++ ICI_Practica1_v2.0/src/es/ucm/fdi/ici/c2223/practica1/grupo/pacman/directivas/IrPuntoMedioConPacman.java
@@ -1,13 +1,32 @@
 package es.ucm.fdi.ici.c2223.practica1.grupo.pacman.directivas;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import es.ucm.fdi.ici.c2223.practica1.grupo.pacman.game_link.GameContainer;
+import pacman.game.Constants.DM;
+import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
 
-public class IrPuntoMedioConPacman extends Directiva {
 
+public class IrPuntoMedioConPacman extends Directiva {
+	private final int GHOSTS_NUMBER = 4;
 	@Override
 	public MOVE getMove() {
-		// TODO Auto-generated method stub
+		Map<GHOST, Double> HalfWay = new HashMap<>();
+		int [] ghostPositions = new int[GHOSTS_NUMBER];
+		int i=0;
+		for (GHOST ghostType : GHOST.values()) {
+			ghostPositions[i]= GameContainer.get().getGhostCurrentNodeIndex(ghostType);
+			Integer pacmanIndex = GameContainer.get().getPacmanCurrentNodeIndex();
+			Integer closestNode = GameContainer.get().getClosestNodeIndexFromNodeIndex(pacmanIndex,ghostPositions, DM.PATH);
+			Double distance = GameContainer.get().getDistance(closestNode, GameContainer.get().getPacmanCurrentNodeIndex(),DM.PATH); 
+			HalfWay.put(ghostType, distance/2);
+			GameContainer.get()
+			GameContainer.get().getApproximateNextMoveAwayFromTarget(ghostPositions[i], pacmanIndex, GameContainer.get().getGhostLastMoveMade(ghostType), DM.PATH);
+			i++;
+		}
 		return null;
 	}
-
+	//camino desde el nodo delante de pacman/ camino minimo entre los dos/ ir al punto medio 
 }
