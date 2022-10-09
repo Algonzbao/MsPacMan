5a0eb490677169f995e899cb096659cef8aeeeaa
Andrés Cardenal Antón <andrescardenalanton@gmail.com>
2022-10-09 19:31:12 +0200

    cambios

--- ICI_Practica1_v2.0/src/es/ucm/fdi/ici/c2223/practica1/grupo/pacman/pacman_directives/EvitarFantasma.java
+++ ICI_Practica1_v2.0/src/es/ucm/fdi/ici/c2223/practica1/grupo/pacman/pacman_directives/EvitarFantasma.java
@@ -1,22 +1,52 @@
 package es.ucm.fdi.ici.c2223.practica1.grupo.pacman.pacman_directives;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import es.ucm.fdi.ici.c2223.practica1.grupo.pacman.game_link.GameContainer;
 import pacman.game.Constants.DM;
 import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
 
-public class EvitarFantasma extends Directiva {
-
+public class EvitarFantasma extends PacmanDirective {
+	
+	private static final DM dm = DM.PATH;
+	private final int GHOSTS_NUMBER = 4;
+	private final int GHOSTS_FOLLOW_LIMIT = 150;
+	
 	@Override
 	public MOVE getMove() {
-		private boolean isThereCloserGhost(GHOST nearestGhost) {
-			//En caso de que exista algún fantasma no comestible más cerca que yo, me escapo
-			double nodeDistance = game.getDistance(game.getPacmanCurrentNodeIndex(), game.getGhostCurrentNodeIndex(nearestGhost), DM.PATH);
-			for(GHOST g: GHOST.values()) {
-				if(game.getDistance(game.getPacmanCurrentNodeIndex(), game.getGhostCurrentNodeIndex(g), DM.PATH) < nodeDistance &&  g !=nearestGhost && game.isGhostEdible(g) == false) {
-					return true;
-				}
+		//Si me puedo comer un fantasma y es el que más cerca tengo de todos me lo como (max puntos)
+		GHOST edibleGhost = getNearestEdibleGhost(GHOSTS_FOLLOW_LIMIT);
+		if (edibleGhost == null)
+			return null;
+		int pacmanIndex = GameContainer.get().getPacmanCurrentNodeIndex();
+		int edibleGhostIndex = GameContainer.get().getGhostCurrentNodeIndex(edibleGhost);
+		MOVE pacLastMove = GameContainer.get().getPacmanLastMoveMade();
+		return GameContainer.get().getApproximateNextMoveTowardsTarget(pacmanIndex, edibleGhostIndex, pacLastMove, dm);
+	}
+	
+	private GHOST getNearestEdibleGhost(int limit) {
+		int [] ghostPositions = new int[GHOSTS_NUMBER];
+		int i=0;
+		Map<Integer, GHOST> map = new HashMap<>();
+		
+		//guardar la posicion en el mapa de cada uno de los fantasmas comestibles
+		for (GHOST ghostType : GHOST.values()) {
+			if (GameContainer.get().isGhostEdible(ghostType)) {
+				ghostPositions[i] = GameContainer.get().getGhostCurrentNodeIndex(ghostType);
+				map.put(ghostPositions[i], ghostType);
+				i++;
 			}
-			return false;
 		}
+		
+		//revisar el nodo más cercano
+		int pacmanIndex = GameContainer.get().getPacmanCurrentNodeIndex();
+		int closestNode = GameContainer.get().getClosestNodeIndexFromNodeIndex(pacmanIndex, ghostPositions, DM.PATH);
+		
+		GHOST closestEat = null;
+		if (GameContainer.get().getDistance(closestNode, pacmanIndex, dm) <= limit)
+			closestEat = map.get(closestNode);
+		return closestEat;
 	}
 }
