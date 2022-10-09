ae91b3eff63c5840b4caaaf5d1daebda9a12049e
Colevo <algonz22@ucm.es>
2022-10-09 19:31:37 +0200

    punto Medio

--- ICI_Practica1_v2.0/src/es/ucm/fdi/ici/c2223/practica1/grupo/pacman/ghost_directives/IrPuntoMedioConPacman.java
+++ ICI_Practica1_v2.0/src/es/ucm/fdi/ici/c2223/practica1/grupo/pacman/ghost_directives/IrPuntoMedioConPacman.java
@@ -12,22 +12,11 @@ public class IrPuntoMedioConPacman extends GhostDirective {
 	private final int GHOSTS_NUMBER = 4;
 	@Override
 	public MOVE getMove(GHOST ghost) {
-		GameContainer.get().getNextMoveTowardsTarget(GHOSTS_NUMBER, GHOSTS_NUMBER, null, null)
-		GameContainer.get().getShortestPath(GHOSTS_NUMBER, GHOSTS_NUMBER, getMove())
-		Map<GHOST, Double> halfWay = new HashMap<>();
-		int [] ghostPositions = new int[GHOSTS_NUMBER];
-		int i=0;
-		for (GHOST ghostType : GHOST.values()) {
-			ghostPositions[i]= GameContainer.get().getGhostCurrentNodeIndex(ghostType);
+		
 			Integer pacmanIndex = GameContainer.get().getPacmanCurrentNodeIndex();
-			Integer closestNode = GameContainer.get().getClosestNodeIndexFromNodeIndex(pacmanIndex,ghostPositions, DM.PATH);
-			Double distance = GameContainer.get().getDistance(closestNode, GameContainer.get().getPacmanCurrentNodeIndex(),DM.PATH); 
-			halfWay.put(ghostType, distance/2);
-			GameContainer.get()
-			GameContainer.get().getApproximateNextMoveAwayFromTarget(ghostPositions[i], pacmanIndex, GameContainer.get().getGhostLastMoveMade(ghostType), DM.PATH);
-			i++;
-		}
-		return null;
+			Integer ghostIndex = GameContainer.get().getGhostCurrentNodeIndex(ghost);
+			MOVE lastMove = GameContainer.get().getGhostLastMoveMade(ghost);
+			return GameContainer.get().getNextMoveTowardsTarget(ghostIndex, pacmanIndex,lastMove, DM.PATH);
 	}
 	//camino desde el nodo delante de pacman/ camino minimo entre los dos/ ir al punto medio 
 }
