9ab2f6c3a00a2bdf867344b156b313d224c3d5b6
Colevo <algonz22@ucm.es>
2022-10-09 19:53:42 +0200

    Punto medio con Next Junction

--- ICI_Practica1_v2.0/src/es/ucm/fdi/ici/c2223/practica1/grupo/pacman/ghost_directives/IrPuntoMedioConPacman.java
+++ ICI_Practica1_v2.0/src/es/ucm/fdi/ici/c2223/practica1/grupo/pacman/ghost_directives/IrPuntoMedioConPacman.java
@@ -1,9 +1,7 @@
 package es.ucm.fdi.ici.c2223.practica1.grupo.pacman.ghost_directives;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import es.ucm.fdi.ici.c2223.practica1.grupo.pacman.game_link.GameContainer;
+import es.ucm.fdi.ici.c2223.practica1.grupo.pacman.util.Detective;
 import pacman.game.Constants.DM;
 import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
@@ -11,11 +9,12 @@ import pacman.game.Constants.MOVE;
 public class IrPuntoMedioConPacman extends GhostDirective {
 	@Override
 	public MOVE getMove(GHOST ghost) {
-		
 			Integer pacmanIndex = GameContainer.get().getPacmanCurrentNodeIndex();
+			MOVE pacmanMove = GameContainer.get().getPacmanLastMoveMade();
+			Integer nextJunct = Detective.nextJunction(pacmanIndex, pacmanMove);
 			Integer ghostIndex = GameContainer.get().getGhostCurrentNodeIndex(ghost);
 			MOVE lastMove = GameContainer.get().getGhostLastMoveMade(ghost);
-			return GameContainer.get().getNextMoveTowardsTarget(ghostIndex, pacmanIndex,lastMove, DM.PATH);
+			return GameContainer.get().getNextMoveTowardsTarget(ghostIndex, nextJunct ,lastMove, DM.PATH);
 	}
 	//camino desde el nodo delante de pacman/ camino minimo entre los dos/ ir al punto medio 
 }
